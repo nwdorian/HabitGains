6960b8bfa9a849e984840c7b31fb284855c7e377
nwdorian <neuwirthdorian@gmail.com>
2026-01-08 01:05:47 +0100

    add Update method to Habit repository

--- src/HabitGains.Infrastructure/Database/Repositories/HabitRepository.cs
+++ src/HabitGains.Infrastructure/Database/Repositories/HabitRepository.cs
@@ -144,6 +144,24 @@ public class HabitRepository(IDbConnectionFactory connectionFactory) : IHabitRep
         await command.ExecuteNonQueryAsync(cancellationToken);
     }
 
+    public async Task Update(Guid id, Habit habit, CancellationToken cancellationToken)
+    {
+        using SqliteConnection connection = await connectionFactory.CreateConnectionAsync(cancellationToken);
+        using SqliteCommand command = connection.CreateCommand();
+
+        command.CommandText = """
+            UPDATE habit
+            SET Name = @Name, Measurement = @Measurement, Favorite = @Favorite, UpdatedAt = @UpdatedAt 
+            WHERE Id = @Id
+            """;
+
+        command.Parameters.AddWithValue("@Id", habit.Id);
+        command.Parameters.AddWithValue("@Name", habit.Name);
+        command.Parameters.AddWithValue("@Measurement", habit.Measurement);
+        command.Parameters.AddWithValue("@Favorite", habit.Favorite);
+        command.Parameters.AddWithValue("@UpdatedAt", habit.UpdatedAt);
+    }
+
     public async Task<bool> IsNameUnique(string name, CancellationToken cancellationToken)
     {
         using SqliteConnection connection = await connectionFactory.CreateConnectionAsync(cancellationToken);
